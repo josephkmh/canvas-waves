c32d682fe7be09766174ee29abf9ef0617589875
joey <jmarshmenthowell@gmail.com>
2019-05-03 11:40:17 +0200

    add demo, expose waves registry

--- src/canvas-waves.js
+++ src/canvas-waves.js
@@ -1,16 +1,15 @@
 import Wave from "./Wave";
 
 const CanvasWaves = () => {
-
   const waves = [];
 
   const CanvasWave = (selector, options) => {
     const wave = new Wave(selector, options);
     waves.push(wave);
     return wave;
-  }
+  };
 
-  return CanvasWave;
-}
+  return { wave: CanvasWave, waves };
+};
 
 export default CanvasWaves();
