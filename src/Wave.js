1d45e02c9ad0948b97c758a3ccad136ac6ba3c48
Joey Marshment-Howell <jmarshmenthowell@gmail.com>
2019-08-14 20:39:25 +0200

    remove waveCount and freezeAfter because they weren't working and are not necessary yet

--- src/Wave.js
+++ src/Wave.js
@@ -1,51 +1,62 @@
 import Node from "./Node.js";
 import Canvas from "./Canvas.js";
 
-function Wave(selector, {
-  baseHeight = 0,
-  nodeCount,
-  speed = 10000,
-  color = "#000000",
-  gradient = false,
-  freezeAfter = false,
-  horizontalOffset = 0,
-  startFlat = false,
-  opacity = 1,
-  waveGrows = false,
-  waveCount = false,
-  waveHeight = "15%",
-  wavesVisible = .5,
-  waveAngle = 0
-} = {}) {
+function Wave(
+  selector,
+  {
+    baseHeight = 0,
+    nodeCount,
+    speed = 10000,
+    color = "#000000",
+    gradient = false,
+    horizontalOffset = 0,
+    startFlat = false,
+    opacity = 1,
+    waveGrows = false,
+    waveHeight = "15%",
+    wavesVisible = 0.5,
+    waveAngle = 0
+  } = {}
+) {
   this.animating = null;
-  this.freezeAfter = freezeAfter;
   this.horizontalOffsetTime = horizontalOffset * speed;
   this.speed = speed;
   this.startFlat = startFlat;
   this.startTime;
   this.waveAngleRadians = waveAngle * (Math.PI / 180);
-  this.waveCount = waveCount;
   this.waveGrows = waveGrows;
 
   /**
    * Set up the canvas & context
    */
-  this.canvas = Canvas({selector, gradient, color, opacity});
+  this.canvas = Canvas({ selector, gradient, color, opacity });
 
   const numberOfNodes = nodeCount || Math.floor(this.canvas.width / 30);
   this.nodes = [];
   for (let i = 0; i < numberOfNodes; i++) {
-    this
-      .nodes
-      .push(new Node({numberOfNodes, canvasWidth: this.canvas.width, canvasHeight: this.canvas.height, wavesVisible: wavesVisible, index: i}));
+    this.nodes.push(
+      new Node({
+        numberOfNodes,
+        canvasWidth: this.canvas.width,
+        canvasHeight: this.canvas.height,
+        wavesVisible: wavesVisible,
+        index: i
+      })
+    );
   }
   this.spaceBetweenNodes = this.canvas.width / (this.nodes.length - 1);
 
   /**
    * Parse user provided values into pixel values (in case a string percentage is provided)
    */
-  this.pixelWaveHeight = this.userInputToPixelValue(waveHeight, this.canvas.height);
-  this.pixelBaseHeight = this.userInputToPixelValue(baseHeight, this.canvas.height);
+  this.pixelWaveHeight = this.userInputToPixelValue(
+    waveHeight,
+    this.canvas.height
+  );
+  this.pixelBaseHeight = this.userInputToPixelValue(
+    baseHeight,
+    this.canvas.height
+  );
 
   /**
    * Draw initial position of wave
@@ -58,21 +69,13 @@ Wave.prototype = {
   /**
    * Animate the wave
    */
-  animate: function () {
+  animate: function() {
     // halt execution if animation has been turned off
     if (this.animating == false) {
       return;
     }
     this.animating = true;
 
-    // stop animation if freezeAfter was set and has been exceeded
-    if (this.freezeAfter && progress > this.freezeAfter) 
-      return;
-    
-    // stop animation if waveCount was set and has been exceeded
-    if (this.waveCount && progress / this.speed >= this.waveCount) 
-      return;
-    
     // update nodes, repaint canvas and request next animation frame
     this.updateNodes();
     this.draw();
@@ -82,16 +85,14 @@ Wave.prototype = {
   /**
    * Draw the current nodes on the canvas context
    */
-  draw: function () {
+  draw: function() {
     let ctx = this.canvas.ctx;
     ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     ctx.beginPath();
     ctx.moveTo(0, this.canvas.height);
-    this
-      .nodes
-      .forEach(node => {
-        ctx.lineTo(node.x, node.y);
-      });
+    this.nodes.forEach(node => {
+      ctx.lineTo(node.x, node.y);
+    });
     ctx.lineTo(this.canvas.width, this.canvas.height);
     ctx.lineTo(0, this.canvas.height);
     ctx.closePath();
@@ -101,25 +102,27 @@ Wave.prototype = {
   /**
    * Calculate the current progress in radians
    */
-  getProgressRadians: function () {
+  getProgressRadians: function() {
     let currentTime = this.timestamp();
     if (!this.startTime && this.animating) {
       this.startTime = currentTime;
     }
     let startTime = this.startTime || currentTime;
-    let timeElapsed = (currentTime - startTime + this.horizontalOffsetTime) % this.speed;
-    let progress = (timeElapsed / this.speed);
+    let timeElapsed =
+      (currentTime - startTime + this.horizontalOffsetTime) % this.speed;
+    let progress = timeElapsed / this.speed;
     return progress * 2 * Math.PI;
   },
 
-  getProgressPercentage: function () {
+  getProgressPercentage: function() {
     // calculate the current progress from 0-1
     let currentTime = this.timestamp();
     if (!this.startTime && this.animating) {
       this.startTime = currentTime;
     }
-    let timeElapsed = (currentTime - this.startTime + this.horizontalOffsetTime) % this.speed;
-    let progress = (timeElapsed / this.speed);
+    let timeElapsed =
+      (currentTime - this.startTime + this.horizontalOffsetTime) % this.speed;
+    let progress = timeElapsed / this.speed;
     return progress;
   },
 
@@ -133,7 +136,7 @@ Wave.prototype = {
   /**
    * Toggle animation of the wave
    */
-  toggleAnimation: function () {
+  toggleAnimation: function() {
     if (!this.animating) {
       this.animating = true;
       this.animate();
@@ -147,27 +150,28 @@ Wave.prototype = {
   /**
    * Update nodes based on current time
    */
-  updateNodes: function () {
+  updateNodes: function() {
     let progress = this.getProgressRadians();
 
-    this
-      .nodes
-      .forEach(node => {
+    this.nodes.forEach(node => {
+      // if (this.startFlat && progress < nodePositionMultiplier) {   node.y =
+      // this.canvasHeight - this.baseHeight;   return }
 
-        // if (this.startFlat && progress < nodePositionMultiplier) {   node.y =
-        // this.canvasHeight - this.baseHeight;   return }
+      let offset =
+        ((Math.sin(progress - node.numberOffset) + 1) / 2) *
+        this.pixelWaveHeight;
+      if (this.waveGrows) {
+        offset =
+          offset * (node.index / (this.nodes.length - 1)) * this.waveGrows;
+      }
+      if (this.waveAngleRadians) {
+        offset +=
+          Math.tan(this.waveAngleRadians) * this.spaceBetweenNodes * node.index;
+      }
+      let yValue = this.canvas.height - offset - this.pixelBaseHeight;
 
-        let offset = ((Math.sin(progress - node.numberOffset) + 1) / 2) * this.pixelWaveHeight;
-        if (this.waveGrows) {
-          offset = offset * (node.index / (this.nodes.length - 1)) * this.waveGrows;
-        }
-        if (this.waveAngleRadians) {
-          offset += Math.tan(this.waveAngleRadians) * this.spaceBetweenNodes * node.index;
-        }
-        let yValue = this.canvas.height - offset - this.pixelBaseHeight;
-
-        node.y = yValue;
-      });
+      node.y = yValue;
+    });
   },
 
   /**
@@ -183,6 +187,6 @@ Wave.prototype = {
       return percentageOf * percentage;
     }
   }
-}
+};
 
 export default Wave;
